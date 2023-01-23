6b73ac9fd6f2cd67a25fb1a76fa76318df19e7e3
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-01-23 04:18:26 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,47 @@
 #include "sort.h"
 
-/*
- *
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *focus, *prev, *tmp;
-	
+	listint_t *iter, *insert, *tmp;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	focus = (*list)->next;
-	while (focus != NULL)
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		prev = focus->prev;
-		tmp = focus->next;
-		while (prev != NULL && focus->n < prev->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			prev->next = focus->next;
-			if (focus->next != NULL)
-				focus->next->prev = prev;
-			focus->next = prev->prev;
-			focus->prev = prev;
-			if (prev->prev != NULL)
-				prev->prev->next = focus;
-			else
-				*list = focus;
-			prev->prev = focus;
-			prev = focus->prev;
+			swap_nodes(list, &insert, iter);
 			print_list((const listint_t *)*list);
 		}
-		focus = tmp;
 	}
-}
